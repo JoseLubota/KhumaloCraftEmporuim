0e8fa1542223c527d155bcf20241567f18f4b06a
Jose Lubota <133894595+JoseLubota@users.noreply.github.com>
2024-03-15 15:20:23 +0200

    Add project files.

---
+++ WebApplication2/Models/userTBL.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace WebApplication2.Models
+{
+    public class userTBL : Controller
+    {
+        public static string conString = "Server=tcp:clvd-sql-server5.database.windows.net,1433;Initial Catalog=clvd-db;Persist Security Info=False;User ID=Jose;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+
+        public static SqlConnection con = new SqlConnection(conString);
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
